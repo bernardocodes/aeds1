edc515e16d8814c78d174ea99e011edae4dfc18e
Bernardo <bernardo.polaki@gmail.com>
2024-02-09 16:27:35 -0300

    Update main.c

--- 2-aeds1-u2-listaCondicional/ex05-listaCondicional/main.c
+++ 2-aeds1-u2-listaCondicional/ex05-listaCondicional/main.c
@@ -1,5 +1,13 @@
+/*5.
+Faça um programa que mostre o menu de opções a seguir, receba a opção do usuário e os dados necessários para executar cada operação.
+Menu de opções:
+1.
+Somar dois números.
+2.
+Raiz quadrada de um número.
+Digite a opção desejada:*/
+
 #include<math.h>
-#include <stdio.h>
 
 int main()
 {
