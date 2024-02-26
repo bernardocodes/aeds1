072fee92a22a419891cca54e35ae136880a09965
Bernardo <121183049+bernardocodes@users.noreply.github.com>
2024-02-26 17:29:08 -0300

    Create lab-26-02-24.c

---
+++ 0-labAeds1/lab-26-02-24.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int nota, res;//entrada
+    scanf("%d",&nota);
+  
+    res = 'N' + 5*((nota/60));//aprovado?
+    printf("%c\n",res);
+
+    res = 'N' + 5*(1-(nota/60));//reco?
+    printf("%c\n",res);
+
+    res = 'N' + 5*(nota/45)*(1-(nota/60));//reprovado?
+    printf("%c\n",res);
+
+    return 0;
+}
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()//ex nota par ou impar
+{
+    int nota, res;
+    scanf("%d",&nota);
+    //res = 'N' + 5;para pular 5 letras
+    //res = 'N' + 5*(nota%2);colocar 1-( para inverter de N para S --> par ou impar
+    res = 'N' + 5*(1-(nota%2));
+    printf("%c\n",res);
+
+    return 0;
+}
+
+//aprovado nota>=60
+//reprovado nota<=45
+//para o ex, vc deve copiar o cod acima para cada situação
+//CHAVE: RESTO DA DIVISÃO, QUE VAI DEIXAR 0 OU 1 E PREENCHER O PARENTESES
+
+*/
