10c56172473b099aae784f2a8218c961c2ca31fd
Bernardo <bernardo.polaki@gmail.com>
2024-03-25 13:01:02 -0300

    Create main.c

---
+++ 1-lista_Fe/ex01/main.c
@@ -0,0 +1,18 @@
+/*
+Exercício 1
+
+Considerando a linguagem C, assinale as alternativas que são possíveis nomes para variáveis.
+a) Somatorio b) Somatório c) X d) Tipo.1 e) aux
+f) printf g) valor final h) Tipo_1 i) valor-total j) int
+*/
+/*
+Solução:
+
+nomes corretos: a),c),e),h),i)
+
+b) não pode porque tem acento,
+d) não pode porque possue caractere especial,
+f) não pode porque possue palavra reservada,
+g) não pode porque possue caractere especial,
+j) não pode porque possue palavra reservada,
+*/
