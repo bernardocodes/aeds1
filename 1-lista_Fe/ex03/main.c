387e46a243b4c62685e510161966017dcf8bba42
Bernardo <121183049+bernardocodes@users.noreply.github.com>
2024-03-25 15:56:42 -0300

    Update main.c

--- 1-lista_Fe/ex03/main.c
+++ 1-lista_Fe/ex03/main.c
@@ -1,3 +1,11 @@
+/*
+Exercício 3
+
+João, um aluno de ensino médio, precisa tirar 10 pontos na sua prova final para poder passar na
+etapa. Crie um programa, em Portugol e em C, que receba do usuário a nota de João e, caso seja
+menor do que 10, imprima "reprovado". Caso seja igual ou maior que 10, imprima "aprovado".
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
