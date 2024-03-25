248fcdac14725bf33d5377ea4ba84dc5c7062301
Bernardo <bernardo.polaki@gmail.com>
2024-03-25 13:10:43 -0300

    Create main.c

---
+++ 1-lista_Fe/ex02/main.c
@@ -0,0 +1,20 @@
+/*
+Exercício 2
+Considerando a linguagem C, determine o tipo das variáveis que armazenam os seguintes valores:
+a) 7 b) P c) 3.141592653 d) 78.3 e) 0
+f) 3.3333 g) TRUE h) FALSO i) 128742 j) 0.0
+*/
+/*
+solução:
+
+a)int
+b)char
+c)double
+d)float
+e)int
+f)float
+g)bool
+n)bool
+i)int
+j)float
+*/
