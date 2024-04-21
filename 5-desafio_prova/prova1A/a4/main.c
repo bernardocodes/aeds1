33340a3732b0d4cbcc0a8df83e795cf7b393f57a
Bernardo <bernardo.polaki@gmail.com>
2024-04-21 15:47:14 -0300

    agr ta certo

--- 5-desafio_prova/prova1A/a4/main.c
+++ 5-desafio_prova/prova1A/a4/main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main()//O CÓDIGO SERVE PARA AS DUAS PROVAS
 {
     int lado;
+    int cont=0;//pq conta a partir de 0
 
     scanf("%d%*c",&lado);
 
-    for(int i=0;i<lado;i++){
-        for(int j=0;j<lado;j++){
-            if(j%2==0){//tabuleiro começa com espaço em branco (i==0), para todo i par, linha começa com espaço em branco
-                printf(" ");
-            }else{
+    for(int i=0;i<lado;i++){//percorrer linhas
+        for(int j=0;j<lado;j++){//percorrer colunas
+
+            if(cont%2==0){//tabuleiro começa no branco
+                printf("-");
+                cont++;//variar a cor no prox loop de j
+            }else{//preto
                 printf("*");
+                cont++;//variar a cor no prox loop de j
             }
         }
         printf("\n");//passar prox linha
+        if(lado%2==0){//atrasar var em - 1 para que o tabuleiro fique quadriculado, e não cheio de linhas
+            cont-=1;
+        }
+        //se for impar n precisa atrasar
     }
+
     return 0;
 }
