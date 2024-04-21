61a66ea5fd8822e6816dd44bcbae4b4554976c8b
Bernardo <bernardo.polaki@gmail.com>
2024-04-21 18:44:51 -0300

    Update main.c

--- 5-desafio_prova/prova1A/a6/main.c
+++ 5-desafio_prova/prova1A/a6/main.c
@@ -6,30 +6,49 @@ int main()
     int n;
     int aux;
 
-    int divisor=0;
-    int dividendo;
+    int qtd_div=0;//zero pq faremos uma contagem
+
+    int divisor;//var calc do resto sem operador % (segundo a fórmula deduzida: resto == divisor - (quociente * dividendo)
     int resto;
     int quociente;
+    int dividendo;
 
     scanf("%d%*c",&n);
 
-    aux=n;
+    aux=n;//manipular n sem alterar o valor original
 
     while(aux>=0){
-        if((n-aux)%2==0){//2
-            divisor++;
+
+        divisor=n-aux;//cálculo do divisor
+        resto=0;//zero pq faremos comparações quando as divisões derem resto zero
+
+        //((n-aux)-(2*((n-aux)/2))==0
+
+        dividendo=2;//divisor de 2?
+        quociente=(n-aux)/2;
+        if((divisor-(dividendo*quociente))==resto){//2 dividendo = 2
+            qtd_div++;
         }
-        if((n-aux)%3==0){//3
-            divisor++;
+
+        dividendo=3;//divisro de 3?
+        quociente=(n-aux)/3;
+        if((divisor-(dividendo*quociente))==resto){//3 dividendo = 3
+            qtd_div++;
         }
-        if((n-aux)%5==0){//5
-            divisor++;
+
+        dividendo=5;//divisor de 5?
+        quociente=(n-aux)/5;
+        if((divisor-(dividendo*quociente))==resto){//5 dividendo = 5
+            qtd_div++;
         }
-        if(divisor==3){//divisivel pelos 3 simultaneamente
+
+        if(qtd_div==3){//divisivel pelos 3 simultaneamente
             printf("%d ",n-aux);
         }
+
         aux--;//pula para o prox num
-        divisor=0;//atualiza para o prox loop
+        qtd_div=0;//atualiza para o prox loop
     }
+
     return 0;
 }
