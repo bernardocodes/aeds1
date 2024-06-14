112c3a426816f7bcb93e3d46c7258a4f13c9e146
Bernardo <bernardo.polaki@gmail.com>
2024-06-14 13:28:13 -0300

    Update main.c

--- 0-labAeds1/#lab_organizado/lab04-calcPoucoEstranha/main.c
+++ 0-labAeds1/#lab_organizado/lab04-calcPoucoEstranha/main.c
@@ -17,67 +17,48 @@ int main()
     //int sub_expoente=0;
 
     scanf("%d%*c",&x);
-    //scanf("%d%*c",&y);
+    scanf("%d%*c",&y);
 
     //scanf("%d%*c",&opc);
+    opc=1;//provisorio
 
-    divisor=x;//setar o divisor
+    switch(opc){
+    case 1:
 
-    quociente=1;//so para entrar no while
+        divisor=x;//setar o divisor
 
-    while(quociente>0){//qtd div inteiras por 10
-        quociente=divisor/dividendo;
-        divisor=quociente;
-        qtd_div++;
-    }
+        quociente=1;//so para entrar no while
 
-    //printf("qtd div = %d\n",qtd_div);
-
-    divisor=x;//preparar prox div
-    expoente=0;
-
-    for(int i=0;i<qtd_div;i++){
-        quociente=divisor/dividendo;//executar a divisão
-        resto=divisor%dividendo;
-        divisor=quociente;//prepara o prox divisor
-
-        if(expoente==0){
-            construtor+=(resto*1);
-            //printf("construtor = %d\n",construtor);
-            expoente++;
-        }else{
-            for(int j=0;j<expoente;j++){//calcula a potencia de 10 de acordo com i
-                pot_10*=10;
-                printf("pot = %d\n",pot_10);
-                construtor+=(resto*pot_10);
-            }
-            expoente++;
+        while(quociente>0){//qtd div inteiras por 10
+            quociente=divisor/dividendo;
+            divisor=quociente;
+            qtd_div++;
         }
 
+        divisor=x;//preparar prox div
+        expoente=0;
 
+        for(int i=0;i<qtd_div;i++){
+            quociente=divisor/dividendo;//executar a divisão
+            resto=divisor%dividendo;
+            divisor=quociente;//prepara o prox divisor
 
-
-        /*
-        if(expoente==0){
-            if(expoente!=y){//construir o num novamente
-                expoente++;
-                construtor+=(resto*1);//10^0==1
-            }
-        }else{
-            for(int j=0;j<expoente;j++){//calcula a potencia de 10 de acordo com i
+            pot_10=1;
+           for(int j=0;j<expoente;j++){//calcula a potencia de 10 de acordo com i
                 pot_10*=10;
-                printf("pot = %d\n",pot_10);
+                printf("pot %d\n",pot_10);
             }
+            construtor+=resto*pot_10;
+            expoente++;
 
-            if(expoente!=y){//construir o num novamente
-                expoente++;
-                construtor+=(resto*pot_10);
-            }
         }
-        */
+        printf("%d\n",construtor);
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
     }
-
-    printf("%d\n",construtor);
 
     return 0;
 }
